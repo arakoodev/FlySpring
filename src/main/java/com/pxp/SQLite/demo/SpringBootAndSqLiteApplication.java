540698057073ff55563e7903662003d24509d141
Sandeep <sss@lambdacurry.com>
2022-10-19 00:40:11 +0530

    https://stackoverflow.com/a/74061738/112050
    added ArgumentResolver
    updated java 17
    added logger

--- src/main/java/com/pxp/SQLite/demo/SpringBootAndSqLiteApplication.java
+++ src/main/java/com/pxp/SQLite/demo/SpringBootAndSqLiteApplication.java
@@ -1,11 +1,12 @@
 package com.pxp.SQLite.demo;
 
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-
 public class SpringBootAndSqLiteApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pxp/SQLite/demo/controller/APIController.java
+++ src/main/java/com/pxp/SQLite/demo/controller/APIController.java
@@ -3,120 +3,135 @@ package com.pxp.SQLite.demo.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pxp.SQLite.demo.InjectLogger;
+import com.pxp.SQLite.demo.Resolver.ArkRequest;
 import com.pxp.SQLite.demo.entity.PersonEntity;
 import com.pxp.SQLite.demo.repository.PersonRepo;
+//import org.apache.log4j.Logger;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-
+/*
+ * Created By: baseer
+ * Project: Fiver
+ * Dated: 15-10-2022 0950 pm
+ *  *Stackoverflow:
+ *  * https://stackoverflow.com/questions/73996101/spring-argumentresolver-to-get-pathvariable-map-and/74061738#74061738
+ *  * https://stackoverflow.com/questions/6351082/using-java-annotation-to-inject-logger-dependency
+ */
 @RestController
 @RequestMapping("/API")
 public class APIController {
 
     @Autowired
     PersonRepo iPersonRepo;
+    public static @InjectLogger Logger log;
 
-    @RequestMapping("/")
-    public String helloWorld(){
-        return "Hello World from Spring Boot";
-    }
+    @RequestMapping(value = { "/HeloWorld/{name}/{id}" }, method = RequestMethod.GET)
+    public static ResponseEntity<JsonNode> get(ArkRequest request) throws JsonProcessingException {
 
-    @RequestMapping(value = {"/article", "/article/{id}", "/article/{name}"} , method= RequestMethod.GET,
-            consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<JsonNode> get (@PathVariable Map<String, String> pathVarsMap,
-                                         @RequestParam MultiValueMap<String,String> test, @RequestBody(required = false) JsonNode
-                                                 requestBody) throws JsonProcessingException {
+        log.info("Hello World Debugging log");
         ObjectMapper mapper = new ObjectMapper();
         JsonNode json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
-        String id  = pathVarsMap.get("id");
-        String name  = pathVarsMap.get("name");
-        // Code to parse multiplemap list
-
-        Set<String> keys = test.keySet();
-
-        for (String key : keys) {
-            System.out.println("Key = " + key);
-            System.out.println("Values = " + test.get(key));
-            List<String> list = (List<String>) test.get(key);
-
-            System.out.println("id : " +list.get(0));
-            System.out.println("name : " + list.get(1));
+        try {
+            if (request.getPathVariables().get("name") != null || request.getPathVariables().get("id") != null) {
+                log.info("Both parameters OK");
+                 json = mapper.readTree("{\"ID\":  \"" + request.getPathVariables().get("name") + "\", \"name\": \"" + request.getPathVariables().get("id")  + "\"}");
+            } else {
+                log.warn("Null parameter warning!");
+                json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
+            }
         }
-        if( id == null && name == null)
-            return ResponseEntity.ok( mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}"));
-
-        Optional<PersonEntity> pr =  iPersonRepo.findById(Integer.parseInt(id));
-        pr.get().setId(Integer.parseInt(id));
-        pr.get().setName(name);
-        json = mapper.readTree("{\"ID\":  \""+pr.get().getId()+"\", \"name\": \""+pr.get().getName()+"\"}");
+        catch(Exception e){
+            log.error("Exception in Hello World");
+        }
 
         return ResponseEntity.ok(json);
     }
 
-    // INSERT Data in DB
-    @RequestMapping(value = {"/article", "/article/{id}", "/article/{name}"} , method= RequestMethod.POST,
-            consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<JsonNode> PostData (@PathVariable Map<String, String> pathVarsMap,
-                                              @RequestParam MultiValueMap<String,String> test, @RequestBody(required = false) JsonNode
-                                                      requestBody) throws JsonProcessingException {
+    @RequestMapping(value = { "/Data/{name}/{id}" }, method = RequestMethod.GET)
+    public ResponseEntity<JsonNode> Fetch (ArkRequest request) throws JsonProcessingException {
+
         ObjectMapper mapper = new ObjectMapper();
         JsonNode json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
-        String id  = pathVarsMap.get("id");
-        String name  = pathVarsMap.get("name");
-
-        // Set json in a Object of Entity Type to store in DB
-        PersonEntity p = new PersonEntity();
-        p.setId(Integer.parseInt(id));
-        p.setName(name);
-
-        // Calling Save and flush to save and close db connection of transaction
-        iPersonRepo.saveAndFlush(p);
+        try {
+            if (request.getPathVariables().get("name") != null || request.getPathVariables().get("id") != null) {
+                log.info("Both parameters OK");
+                PersonEntity p = iPersonRepo.getOne(Integer.parseInt(request.getPathVariables().get("id")));
+                json = mapper.readTree("{\"ID\":  \"" + p.getId() + "\", \"name\": \"" + p.getName() + "\"}");
+            } else {
+                log.warn("Null parameter warning!");
+                json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
+            }
+        }
+        catch(Exception e){}
 
         return ResponseEntity.ok(json);
     }
 
-    // Update data on base on ID
-    @RequestMapping(value = {"/article", "/article/{id}", "/article/{name}"} , method= RequestMethod.PUT,
-            consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<JsonNode> UpdateData (@PathVariable Map<String, String> pathVarsMap,
-                                                @RequestParam MultiValueMap<String,String> test, @RequestBody(required = false) JsonNode
-                                                        requestBody) throws JsonProcessingException {
+    @RequestMapping(value = { "/Data/{name}/{id}" }, method = RequestMethod.POST)
+    public ResponseEntity<JsonNode> Save(ArkRequest request) throws JsonProcessingException{
+
         ObjectMapper mapper = new ObjectMapper();
         JsonNode json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
-        String id  = pathVarsMap.get("id");
-        String name  = pathVarsMap.get("name");
+        if( request.getPathVariables().get("name") != null ||  request.getPathVariables().get("id") != null) {
 
-        Optional<PersonEntity> pr =  iPersonRepo.findById(Integer.parseInt(id));
-        pr.get().setId(Integer.parseInt(id));
-        pr.get().setName(name);
+            log.info("Both parameters OK");
+            PersonEntity p = new PersonEntity();
+            p.setId(Integer.parseInt(request.getPathVariables().get("id")));
+            p.setName(request.getQueryParameters().get("test").get(0));
 
-        iPersonRepo.saveAndFlush(pr.get());
+            iPersonRepo.saveAndFlush(p);
 
-        return ResponseEntity.ok(json);
+            json = mapper.readTree("{\"ID\":  \""+ p.getId() +"\", \"name\": \""+p.getName()+"\"}");
+        }
+        else
+        {
+            log.warn("Null parameter warning!");
+            json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
+        }
+       return ResponseEntity.ok(json);
     }
 
+    @RequestMapping(value = { "/Data/{name}/{id}" }, method = RequestMethod.PUT)
+    public ResponseEntity<JsonNode> Update(ArkRequest request) throws JsonProcessingException{
 
-    @RequestMapping(value = {"/article", "/article/{id}", "/article/{name}"} , method= RequestMethod.DELETE,
-            consumes= MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<JsonNode> DeleteData (@PathVariable Map<String, String> pathVarsMap,
-                                                @RequestParam MultiValueMap<String,String> test, @RequestBody(required = false) JsonNode
-                                                        requestBody) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
-        String id  = pathVarsMap.get("id");
-        String name  = pathVarsMap.get("name");
+        if( request.getPathVariables().get("name") != null ||  request.getPathVariables().get("id") != null) {
 
-        iPersonRepo.deleteById(Integer.parseInt(id));
+            log.info("Both parameters OK");
+            PersonEntity p = iPersonRepo.getOne(Integer.parseInt(request.getPathVariables().get("id")));
+            p.setName(request.getQueryParameters().get("test").get(0));
 
+            iPersonRepo.saveAndFlush(p);
+            json = mapper.readTree("{\"ID\":  \""+ p.getId() +"\", \"name\": \""+p.getName()+"\"}");
+        }
+        else {
+            log.warn("Null parameter warning!");
+            json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
+        }
         return ResponseEntity.ok(json);
     }
 
+    @RequestMapping(value = { "/Data/{name}/{id}" }, method = RequestMethod.DELETE)
+    public ResponseEntity<JsonNode> Delete(ArkRequest request) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
+        if( request.getPathVariables().get("name") != null ||  request.getPathVariables().get("id") != null) {
+
+            log.info("Both parameters OK");
+            PersonEntity p = iPersonRepo.getOne(Integer.parseInt(request.getPathVariables().get("id")));
+            json = mapper.readTree("{\"ID\":  \""+ p.getId() +"\", \"name\": \""+p.getName()+"\"}");
+        }
+        else {
+            log.warn("Null parameter warning!");
+            json = mapper.readTree("{\"ID\":  \"Not Found\", \"name\": \"Not Found\"}");
+        }
+        iPersonRepo.deleteById(Integer.parseInt(request.getPathVariables().get("id")));
+
+        return ResponseEntity.ok(json);
+    }
 
 }
