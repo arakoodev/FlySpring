3d88d2e0fb61a48bbf2a3a939df125197ac5f15c
Sandeep <sss@lambdacurry.com>
2022-10-22 23:13:52 +0530

    updated to 2.7.4 prepping for graalvm spring 3

---
+++ src/main/java/com/pxp/SQLite/demo/InjectLogger.java
@@ -0,0 +1,8 @@
+package com.pxp.SQLite.demo;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InjectLogger {
+}
---
+++ src/main/java/com/pxp/SQLite/demo/Resolver/ArkRequest.java
@@ -0,0 +1,54 @@
+package com.pxp.SQLite.demo.Resolver;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Map;
+
+/*
+ * Created By: baseer
+ * Project: Fiver
+ * Dated: 15-10-2022 0950 pm
+ * Stackoverflow: https://stackoverflow.com/questions/73996101/spring-argumentresolver-to-get-pathvariable-map-and/74061738#74061738
+ */
+public class ArkRequest {
+
+        public Map<String, String> pathVariables;
+        public MultiValueMap<String, String> queryParameters;
+        public JsonNode requestBody;
+
+
+        ArkRequest() {
+        }
+
+        public ArkRequest(Map<String, String> pathVarsMap , MultiValueMap<String , String> mvm, JsonNode rbody) {
+            this.pathVariables = pathVarsMap;
+            this. queryParameters = mvm;
+            this.requestBody = rbody;
+
+        }
+
+        public Map<String, String> getPathVariables() {
+                return pathVariables;
+        }
+
+        public void setPathVariables(Map<String, String> pathVariables) {
+                this.pathVariables = pathVariables;
+        }
+
+        public MultiValueMap<String, String> getQueryParameters() {
+                return queryParameters;
+        }
+
+        public void setQueryParameters(MultiValueMap<String, String> queryParameters) {
+                this.queryParameters = queryParameters;
+        }
+
+        public JsonNode getRequestBody() {
+                return requestBody;
+        }
+
+        public void setRequestBody(JsonNode requestBody) {
+                this.requestBody = requestBody;
+        }
+}
