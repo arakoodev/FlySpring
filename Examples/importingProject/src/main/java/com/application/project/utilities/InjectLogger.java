8bf3a6415d392818c1276313c456aa95af149af1
Ans-Elnagar <ansgom3a@gmail.com>
2023-02-15 21:22:29 +0200

    clearing Examples folder

--- Examples/importingProject/src/main/java/com/application/project/utilities/InjectLogger.java
+++
@@ -1,8 +0,0 @@
-package com.application.project.utilities;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface InjectLogger {
-}
--- Examples/readme.txt
+++
@@ -1,3 +0,0 @@
-This is just an example project so the cred inside has no meaning
-user with EDITOR role is testing@test.com password Ab123456
-user without roles is normal@users.com password Ab123456
