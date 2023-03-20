9b43441d680a73348c24d76cd0ffacb20fa806e6
Rohan Guha <rohanguha9916@gmail.com>
2023-03-20 23:05:46 +0530

    Deleted clutter and stale files. (#51)
    Deleted htmx folder (as it was unusable anyways [kotlin])
    examples/starter --> starter.
    Project is lean and clean now and everything is working as before.

--- htmx spring creativetim/src/main/java/com/htmx/thymeleaf/users/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.htmx.thymeleaf.users;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
---
+++ starter/src/main/java/com/application/project/myapi/Flymsg.java
@@ -0,0 +1,13 @@
+package com.application.project.myapi;
+
+import com.flyspring.autoroute.FlyRequest;
+import com.flyspring.autoroute.annotations.PathVariableAnnotation;
+
+public class Flymsg {
+
+    @PathVariableAnnotation(name = { "{msg}" })
+    public String flyget(FlyRequest request){
+        return request.getPathVariable("msg");
+    }
+    
+}
